625c60d990570ea46bb2c8e127714d4336c54d26
Noah Surprenant <noah.surprenant@gmail.com>
2019-09-10 02:28:55 -0400

    Starting A Session

--- PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
@@ -3,6 +3,10 @@
 
 #include "LobbyGameMode.h"
 
+#include "TimerManager.h"
+
+#include "PuzzlePlatformsGameInstance.h"
+
 void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 {
 	Super::PostLogin(NewPlayer);
@@ -10,11 +14,7 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 
 	if (NumberOfPlayers >= 2)
 	{
-		UWorld* World = GetWorld();
-		if (!ensure(World != nullptr)) return;
-
-		bUseSeamlessTravel = true;
-		World->ServerTravel("/Game/Maps/Game?listen");
+		GetWorldTimerManager().SetTimer(GameStartTimer, this, &ALobbyGameMode::StartGame, 30);
 	}
 }
 
@@ -22,4 +22,18 @@ void ALobbyGameMode::Logout(AController* Exiting)
 {
 	Super::Logout(Exiting);
 	--NumberOfPlayers;
+}
+
+void ALobbyGameMode::StartGame()
+{
+	auto GameInstance = Cast<UPuzzlePlatformsGameInstance>(GetGameInstance());
+	if (GameInstance == nullptr) return;
+
+	GameInstance->StartSession();
+
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+
+	bUseSeamlessTravel = true;
+	World->ServerTravel("/Game/Maps/Game?listen");
 }
